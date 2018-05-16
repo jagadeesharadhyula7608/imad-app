10b7f793c06e6fd8a4f38c1b2ebabca2815c46db
jagadeesharadhyula7608 <36735523+jagadeesharadhyula7608@users.noreply.github.com>
2018-05-16 10:44:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
